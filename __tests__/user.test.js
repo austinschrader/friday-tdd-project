8123de9eeefd2889d28416063752a536767045fa
Austin Schrader <austin.jschrader@gmail.com>
2020-11-13 12:12:12 -0800

    Good fail of earthAge and instantiation of object

---
+++ __tests__/user.test.js
@@ -0,0 +1,8 @@
+import User from './../src/js/user.js';
+
+describe('User', () => {
+	test('should correct output the earth age of the User object', () => {
+		const user = new User(45);
+		expect(user.earthAge).toEqual(45);
+	});
+});
