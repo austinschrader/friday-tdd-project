33dabd63ff030337a24390e3c94be6c1d95ef8b5
Austin Schrader <austin.jschrader@gmail.com>
2020-11-13 12:20:35 -0800

    Good fail on mercury age prototype test

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,8 +1,13 @@
 import { User } from './../src/js/user.js';
 
 describe('User', () => {
-	test('should correct output the earth age of the User object', () => {
+	test('should correctly output the Earth age of the User object', () => {
 		const user = new User(45);
 		expect(user.earthAge).toEqual(45);
+	});
+
+	test('should correctly output the Mercury age of the User object', () => {
+		const user = new User(45);
+		expect(user.mercuryAge().toEqual(45 * 0.24));
 	});
 });
