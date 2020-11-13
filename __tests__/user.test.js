32ea52c56d4b5439eb38435c8780c93885928385
Austin Schrader <austin.jschrader@gmail.com>
2020-11-13 12:34:36 -0800

    Passing test for returning mercury age of user object

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -2,12 +2,12 @@ import { User } from './../src/js/user.js';
 
 describe('User', () => {
 	test('should correctly output the Earth age of the User object', () => {
-		const user = new User(45);
+		let user = new User(45);
 		expect(user.earthAge).toEqual(45);
 	});
 
 	test('should correctly output the Mercury age of the User object', () => {
-		const user = new User(45);
-		expect(user.mercuryAge().toEqual(45 * 0.24));
+		let user1 = new User(45);
+		expect(user1.mercuryAge()).toEqual(45 * 0.24);
 	});
 });
--- src/js/user.js
+++ src/js/user.js
@@ -2,4 +2,8 @@ export class User {
 	constructor(earthAge) {
 		this.earthAge = earthAge;
 	}
+
+	mercuryAge() {
+		return this.earthAge * 0.24;
+	}
 }
