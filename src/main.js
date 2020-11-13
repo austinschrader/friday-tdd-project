830b2008f17c9f899b2ec758dbf87f27a488d7b8
Austin Schrader <austin.jschrader@gmail.com>
2020-11-13 12:06:54 -0800

    Output hello world on submit button click

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import User from './js/user.js';
+
+$(document).ready(function () {
+	$('#formOne').submit(function (event) {
+		event.preventDefault();
+		console.log('hello world');
+	});
+});
