af8ad2373c2f26e87f124ee5b8560921adf1627b
Austin Schrader <austin.jschrader@gmail.com>
2020-11-13 13:23:45 -0800

    Add the UI

--- src/main.js
+++ src/main.js
@@ -2,11 +2,41 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import User from './js/user.js';
+import { User } from './js/user.js';
 
 $(document).ready(function () {
 	$('#formOne').submit(function (event) {
 		event.preventDefault();
-		console.log('hello world');
+		let lifeExpectancy = parseInt($('#life-expectancy').val());
+		let earthAge = parseInt($('#earth-age').val());
+
+		let user = new User(earthAge, lifeExpectancy);
+		console.log(user);
+		$('#results').empty().append(`<ul> Your Solar Age Information:
+    <li>Earth age: ${user.earthAge}</li>
+    <li>Earth Life Expectancy: ${user.earthYearsLeft(
+			lifeExpectancy - earthAge
+		)}</li>
+    <li>Mercury age: ${user.mercuryAge()}</li>
+    <li>Mercury Life Expectancy: ${user.mercuryYearsLeft(
+			(lifeExpectancy - earthAge) / 0.24
+		)}</li>
+    <li>Venus age: ${user.venusAge()}</li>
+    <li>Venus Life Expectancy: ${user.venusYearsLeft(
+			lifeExpectancy - earthAge
+		)}</li>
+    <li>Mars age: ${user.marsAge()}</li>
+    <li>Mars Life Expectancy: ${user.marsYearsLeft(
+			lifeExpectancy - earthAge
+		)}</li>
+    <li>Jupiter age: ${user.jupiterAge()}</li>
+    <li>Jupiter Life Expectancy: ${user.jupiterYearsLeft(
+			lifeExpectancy - earthAge
+		)} </li>
+    </ul>
+    
+    
+    
+    `);
 	});
 });
