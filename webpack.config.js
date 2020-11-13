a1e9029786122097d766fefedbb614275ac96d5f
Austin Schrader <austin.jschrader@gmail.com>
2020-11-13 11:57:05 -0800

    Update the project name

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
 	plugins: [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
-			title: 'Shape Tracker',
+			title: 'Friday TDD Project',
 			template: './src/index.html',
 			inject: 'body',
 		}),
