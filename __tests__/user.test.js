b818a524de27b3bf2db744a491764aeb1e6ea4be
Austin Schrader <austin.jschrader@gmail.com>
2020-11-13 16:23:31 -0800

    Add in good fail mercuryYearsLeft with negative years left

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -6,13 +6,6 @@ describe('User', () => {
 		expect(user.earthAge).toEqual(45);
 	});
 
-	test('should correctly output the Earth age of the User object', () => {
-		let user = new User(80, 75);
-		expect(user.earthYearsLeft()).toEqual(
-			`youve lived ${Math.abs(75 - 80)} years past your life expectancy`
-		);
-	});
-
 	test('should correctly output the Mercury age of the User object', () => {
 		let user = new User(45);
 		expect(user.mercuryAge()).toEqual(45 / 0.24);
@@ -38,9 +31,23 @@ describe('User', () => {
 		expect(user.earthYearsLeft()).toEqual(78.54 - 45);
 	});
 
+	test('should correctly output the Earth age of the User object', () => {
+		let user = new User(80, 75);
+		expect(user.earthYearsLeft()).toEqual(
+			`youve lived ${Math.abs(75 - 80)} years past your life expectancy`
+		);
+	});
+
 	test('should correctly output the life expectancy left in Mercury years of the User object', () => {
 		let user = new User(45, 78.54);
 		expect(user.mercuryYearsLeft()).toEqual((78.54 - 45) / 0.24);
+	});
+
+	test('should correctly output the Earth age of the User object', () => {
+		let user = new User(80, 75);
+		expect(user.mercuryYearsLeft()).toEqual(
+			`youve lived ${Math.abs(75 - 80) / 0.24} years past your life expectancy`
+		);
 	});
 
 	test('should correctly output the life expectancy left in Venus years of the User object', () => {
